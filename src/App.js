768640f4a9900d710ad4349dffd5244d4c5f112d
DanielaDY <danielayamahoka551@gmail.com>
2023-04-13 19:31:24 -0700

    inicio de sesion (estilos) hecho

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
-import {Button, Card, Dropdown} from 'react-bootstrap/';
+//import {Button, Card, Dropdown} from 'react-bootstrap/';
 import perro1 from './istock.jpg'
 import Registro from './Components/Registro';
-import PrincipalMascotas from './Components/PrincipalMascotas';
+//import PrincipalMascotas from './Components/PrincipalMascotas';
+import InicioSesion from './Components/InicioSesion';
 function App() {
 
   return (
     <div>
-      <PrincipalMascotas/>
+      <InicioSesion/>
     </div>
   );
 }
