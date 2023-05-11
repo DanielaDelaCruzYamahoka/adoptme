bf2a46328dcbcdb70399402b59de31048e8ab8a2
AracelyGarciaArellano <aracely.garcia193@tectijuana.edu.mx>
2023-05-10 23:58:28 -0700

    diseño de contacto[C

--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -1,62 +1,113 @@
 import FooterIndex from './FooterIndex';
 import HeaderII from './HeaderII';
 import Navegacion from './Navegacion';
-import chihuahua from '../chihuahua.png'
+import perroscontacto from '../perroscontacto.png'
 import './EstiloFormularios.css'
+import { Button, Card, Dropdown, InputGroup, Form, Table, Image } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Contacto.css';
 
 function Contacto() {
     return (
-      <div>
-        <HeaderII
-        texto1="Bienvenido"
-        texto2="Contacto"
-        />
-        <Navegacion
-        etiqueta={<a>Aquí van los links de navegacion</a>}/>
-        <main>
-            <section className='Formulario'>
-                <form>
-                    <fieldset>
-                        <div>
-                            <label for="">Nombre: </label>
-                            <input type="text" name="nom" placeholder="Nombre"/>
-                        </div>
-                        <div>
-                            <label for="">Apellido: </label>
-                            <input type="text" name="ape" placeholder="Apellido"/>
-                        </div>
-                        <div>
-                            <label for="">Telefono: </label>
-                            <input type="tel" name="tele" placeholder="Telefono"/>
-                        </div>
-                        <div>
-                            <label for="">Correo: </label>
-                            <input type="email" name="correo" placeholder="mail"/>
-                        </div>
-                        <div>
-                            <label for="">Mensaje</label>
-                            <br/>
-                            <textarea name="mensaje"  cols="30" rows="10"></textarea>
-                        </div>
-                        <div>
-                        <input type="submit" value="Enviar" class="enviar"/>
-                        </div>
-                    </fieldset>
-                </form>
-                <div className="Extra">
+        <div >
+            <HeaderII
+                texto1="Bienvenido"
+                texto2="Contacto"
+            />
+            <Navegacion
+                etiqueta={<a>Aquí van los links de navegacion</a>} />
+            <div className='componen'>
+                <Card >
+                    <Card.Body>
+                        <Card.Title>Contactenos</Card.Title>
+                        <Card.Text>Mandenos un mensaje por este medio y atenderemos su duda.</Card.Text>
+                        <Form /*onSubmit={manejoenvio}*/>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text>Nombre</InputGroup.Text>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Nombre"
+                                    name="nombre"
+                                    id="nombre"
+                                //value={mascotas.id}
+                                //onChange={guardarCambios}
+                                />
+                            </InputGroup>
+
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text>Apellido</InputGroup.Text>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Apellido"
+                                    name="apellido"
+                                    id="apellido"
+                                //value={mascotas.nombre}
+                                //onChange={guardarCambios}
+                                />
+                            </InputGroup>
+
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text>Telefono</InputGroup.Text>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Telefono"
+                                    name="telefono"
+                                    id="telefono"
+                                //value={mascotas.raza}
+                                //onChange={guardarCambios}
+                                />
+                            </InputGroup>
+
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text>Correo</InputGroup.Text>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Correo"
+                                    name="correo"
+                                    id="correo"
+                                //value={mascotas.descripcion}
+                                //onChange={guardarCambios}
+                                />
+                            </InputGroup>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text>Mensaje</InputGroup.Text>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Ingresa el mensaje que nos deseas transmitir"
+                                    name="mensaje"
+                                    id="mensaje"
+                                    style={{ height: "200px", width: "100%" }}
+                                //value={mascotas.descripcion}
+                                //onChange={guardarCambios}
+                                />
+                            </InputGroup>
+                            <br />
+                            <div style={{ display: "flex", justifyContent: "center" }}>
+                                <Button variant="primary" type="submit" >
+                                    Enviar
+                                </Button>
+                            </div>
+                        </Form>
+                    </Card.Body>
+                </Card>
+
+                <div className='extra'>
                     <p>Teléfono</p>
                     <p>+52 6641947541</p>
                     <p>Sigue nuestras redes sociales</p>
-                    <img src={chihuahua}/>
+                    <img style={{	maxWidth: "35rem", height: "25rem"}}src={perroscontacto} />
                 </div>
-            </section>
-        </main>
+            </div>
 
-        <FooterIndex></FooterIndex>
-      </div>
-      
-  
+
+
+
+            <FooterIndex></FooterIndex>
+        </div>
+
+
+
     );
-  }
-  
-  export default Contacto;
+}
+
+export default Contacto;
